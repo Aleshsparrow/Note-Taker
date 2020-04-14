dcb8fabeb2db845d29fd6da924374f851282fc6c
Segun Alesinloye <segunalesh@gmail.com>
2020-04-14 01:56:40 -0500

    Comments

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,10 +3,12 @@ var path = require("path");
 // Seeting up and exporting html routes
 module.exports = function(app){
 
+  // Home route
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   
+  // Nptes route
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
