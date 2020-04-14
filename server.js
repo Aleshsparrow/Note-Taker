59bb46e3fdffd29aa7cc1039b527a69e8a6374f2
Segun Alesinloye <segunalesh@gmail.com>
2020-04-14 01:36:00 -0500

    Added some comments to the server

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
+// Requiring the important npm packages
 var express = require("express");
-// var path = require("path")
+
+// Requiring the api and html routes
 var apiRoutes = require("./routes/apiroutes");
 var htmlRoutes = require("./routes/htmlroutes");
 
-
+// Setting up port for the server
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Giving acces to the server to use the html and the api routes
 app.use(express.static("public"));
-// app.use("/api", apiRoutes);
 htmlRoutes(app);
 apiRoutes(app);
-// app.use("/", htmlRoutes);
 
 
 
-
+// Setting up port for the server
 app.listen(PORT, function() {
     console.log("App listening on: http://localhost:" + PORT);
   });
