bc569ef089fd8c9e9477998d3a5fd8ebd72c551a
Segun Alesinloye <segunalesh@gmail.com>
2020-04-14 01:41:38 -0500

    Added commentts to the html routes file

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,7 +1,6 @@
 var path = require("path");
-// var express = require("express");
 
-// var app = express();
+// Seeting up and exporting html routes
 module.exports = function(app){
 
   app.get("/", function(req, res) {
